fa53deaba76c6ffde8cb2744536c99cdf1021e6d
Pavlo Serediuk <pav.sered@gmail.com>
2019-05-05 17:59:39 +0300

    some initial code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,49 @@
-import { ajax } from 'rxjs/ajax'
-import { map } from 'rxjs/operators'
-import { XMLHttpRequest } from 'xmlhttprequest'
+import { ajax } from 'rxjs/ajax';
+import { concat, interval } from 'rxjs';
+import { map, tap, filter, bufferTime } from 'rxjs/operators';
+import * as R from 'ramda';
 
-console.clear();
-
+import { XMLHttpRequest } from 'xmlhttprequest';
 function createXHR() {
     return new XMLHttpRequest();
 }
 
-const users$ = ajax({
+console.clear();
+
+const ajax$ = ajax({
     createXHR,
-    url: 'https://jsonplaceholder.typicode.com/todos',
+    url: 'https://jsonplaceholder.typicode.com/todos/1',
     method: 'GET'
 }).pipe(
-    map(({ response }) => response)
+    map(R.prop('response'))
 );
 
-const subscribe = users$.subscribe(
+const checkIfBothPartSumAreEqual = numberStr => {
+    const half = parseInt(numberStr.length / 2, 10);
+    const parts = [
+        numberStr.slice(0, half) || numberStr[0],
+        numberStr.slice(half + numberStr.length % 2) || numberStr[0]
+    ].map(R.pipe(
+        R.split(''),
+        R.map(value => +value),
+        R.sum
+    ));
+
+    return parts[0] === parts[1];
+
+};
+
+const other$ = interval(10).pipe(
+    filter(R.pipe(
+        R.toString,
+        checkIfBothPartSumAreEqual,
+    )),
+    bufferTime(500)
+);
+
+const stream$ = concat(other$);
+
+stream$.subscribe(
     console.log,
     console.error
 );
